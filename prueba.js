def4f065e76571aca69b2d2a63bafa4fef27f805
miguel <miguel.sacristan@gmail.com>
2014-04-06 00:07:01 +0100

    starting up

---
+++ prueba.js
@@ -0,0 +1,27 @@
+var Model = Backbone.Model.extend({});
+var mymodel = new Model();         
+var View = Backbone.View.extend({
+	initialize : function (){
+        this.template = Handlebars.compile($("#prueba-template").html());
+	},
+	events : {
+        "click td" : "hey",
+        "click tr" : "heyrow"
+	},
+	hey : function (){
+		console.log("hey there colum");
+	},
+	heyrow : function (){
+		console.log("hey there row");
+	},
+	render : function(){
+		this.$el.html(this.template(this.model.toJSON()));
+	}
+});                             
+
+$(function(){
+
+var myview = new View({model : mymodel, el : $("#tableHolder")});
+myview.render();
+
+});git remote add origin https://github.com/msacristan/try_kinetic.git
